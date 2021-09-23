d2ea555eccba833805deb50a25f9b2161faf7199
Mhttt <74661052+Mhttt@users.noreply.github.com>
2021-09-23 14:05:15 +0200

    Feat/student (#2)
    * add code, tests, and status functionality
    
    Co-authored-by: Ahmed Bassam Galal <ahga@itu.dk>
    Co-authored-by: SgTTrollinator <ahmed.2720@live.dk>
    Co-authored-by: mille <milo@itu.dk>

--- Assignment2.Tests/UnitTest1.cs
+++ Assignment2.Tests/UnitTest1.cs
@@ -1,14 +1,89 @@
 using System;
 using Xunit;
+using Assignment2;
 
 namespace Assignment2.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Status_should_be_new()
         {
+            //Arrange
+            DateTime Now = DateTime.Now;
+            TimeSpan Span = Now.AddMonths(9) - Now;
+            Student Student = new Student(13, "Ahmed", "Galal", DateTime.Now.Subtract(Span),DateTime.Now.AddYears(4),DateTime.Now.AddYears(4));
+            
+            //Act
+            Status Status = Student.Status;
+            
+            Assert.Equal(Status.New,Status);
 
         }
+
+        [Fact]
+        public void Status_should_be_droupout()
+        {
+            //Arrange
+            DateTime Now = DateTime.Now;
+            TimeSpan Span = Now.AddYears(1) - Now;
+            Student Student = new Student(13, "Ahmed", "Galal", DateTime.Now.Subtract(Span*3),DateTime.Now.Subtract(Span*2),DateTime.Now.AddYears(1));
+            
+            //Act
+            Status Status = Student.Status;
+            
+            Assert.Equal(Status.Dropout,Status);
+        }
+        
+        [Fact]
+        public void Status_should_be_active()
+        {
+            //Arrange
+            DateTime Now = DateTime.Now;
+            TimeSpan Span = Now.AddYears(1) - Now;
+            Student Student = new Student(13, "Ahmed", "Galal", DateTime.Now.Subtract(Span),DateTime.Now.AddYears(4),DateTime.Now.AddYears(4));
+            
+            //Act
+            Status Status = Student.Status;
+            
+            Assert.Equal(Status.Active,Status);
+        }
+        
+        [Fact]
+        public void Status_should_be_graduated() 
+        {
+            //Arrange
+            DateTime Now = DateTime.Now;
+            TimeSpan Span = Now.AddYears(1) - Now;
+            DateTime endDate = DateTime.Now.Subtract(Span * 2);
+            Student Student = new Student(13, "Ahmed", "Galal", DateTime.Now.Subtract(Span*5),endDate,endDate);
+            
+            //Act
+            Status Status = Student.Status;
+            
+            Assert.Equal(Status.Graduated,Status);
+        }
+
+        [Fact]
+        public void ToString_returns_right_string()
+        {
+            //arrange
+            String t1 = "Name:Ahmed Galal, ID: 13, Start Date: 01/01/2020, Graduation Date: 01/01/2023, End Date: 01/01/2023, Status: Active";
+
+            DateTime StartDate = new DateTime(2020, 1, 1);
+            DateTime endGraduationDate = new DateTime(2023, 1, 1);
+
+            Student Student = new Student(13, "Ahmed", "Galal", StartDate, endGraduationDate, endGraduationDate);
+
+            //act
+            String t2 = Student.ToString();
+
+            //assert
+            Assert.Equal(t1,t2);
+
+        }
+
+
+        
     }
 }
---
+++ Assignment2/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Assignment2
+{
+    public class Student
+    {
+        public Status Status
+        {
+            get
+            {
+                if (EndDate < DateTime.Now && EndDate < GraduationDate) {return Status.Dropout;}
+                if (DateTime.Now <= startDate.AddYears(1)) {return Status.New;}
+                if (DateTime.Now > startDate && DateTime.Now < GraduationDate) {return Status.Active;}
+                if (DateTime.Now > GraduationDate && GraduationDate <= EndDate) {return Status.Graduated;}
+
+                throw new InvalidOperationException("Please write another date");
+
+            } 
+            
+        }
+        int id;
+        string givenName;
+        string surName;
+        DateTime startDate;
+        public DateTime EndDate { get; set; }
+        public DateTime GraduationDate { get; set; }
+
+        public Student(int id, string givenName, string surName, DateTime startDate, DateTime endDate, DateTime graduationDate){
+            this.id = id;
+            this.givenName = givenName;
+            this.surName = surName;
+            this.startDate = startDate;
+            this.EndDate = endDate;
+            this.GraduationDate = graduationDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Name:{0} {1}, ID: {2}, Start Date: {3:d}, Graduation Date: {4:d}, End Date: {5:d}, Status: {6}", givenName, surName, id, startDate,GraduationDate,EndDate,Status);
+        }
+
+        public static void main(string[] args) 
+        {   
+            DateTime Now = DateTime.Now;
+            TimeSpan Span = Now.AddMonths(9) - Now;
+            Student Student = new Student(13, "Ahmed", "Galal", DateTime.Now.Subtract(Span),DateTime.Now.AddYears(4),DateTime.Now.AddYears(4));
+            Console.WriteLine(Student.ToString());
+        }
+    }
+}
